b627f4d1c69b1ae0a2a2e60b923ec2b3c81cdbe1
Ziga Benko <zigac.benko@gmail.com>
2025-12-10 16:44:36 +0100

    add statistics

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { inject } from '@vercel/analytics'
+
 const app = createApp(App)
+
+inject()
 
 app.use(createPinia())
 app.use(router)
